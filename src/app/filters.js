d5525174e6b40b58f0311a987060c1919b80a380
Germanicus <dawdud12@gmail.com>
2019-05-27 16:39:32 +0200

    'formated code''

--- src/app/filters.js
+++ src/app/filters.js
@@ -1,24 +1,28 @@
 import Posts from "./posts.js";
 
 class Filter {
-  constructor(category){
-    this.category= category;
+  constructor(category) {
+    this.category = category;
   }
   sortByCategory() {
-   
     Posts.getPosts().then(response => {
-     const  sortSwitch= {
-        "scores": () => response.posts.sort((obj1,obj2)=> { return obj1.score - obj2.score}),
-        "upvotes": () => response.posts.sort((obj1,obj2)=> { return obj1.upvotes - obj2.upvotes}),
-        "num_comments": () => response.posts.sort((obj1,obj2)=> obj1.num_comments - obj2.num_comments),
-
-      }
-       console.log(sortSwitch[this.category]());
-     
-      
-     
-    }) ;
+      const sortSwitch = {
+        scores: () =>
+          response.posts.sort((obj1, obj2) => {
+            return obj1.score - obj2.score;
+          }),
+        upvotes: () =>
+          response.posts.sort((obj1, obj2) => {
+            return obj1.upvotes - obj2.upvotes;
+          }),
+        num_comments: () =>
+          response.posts.sort(
+            (obj1, obj2) => obj1.num_comments - obj2.num_comments
+          )
+      };
+      console.log(sortSwitch[this.category]());
+    });
   }
 }
 
-export default new Filter('num_comments');
+export default new Filter("num_comments");
--- src/app/posts.js
+++ src/app/posts.js
@@ -5,34 +5,30 @@ class Posts {
     this.list = list;
   }
   getPosts() {
-    return getRequest()
-      .then(response => {
-        for (const {
-          data: { title, ups, score, num_comments, created_utc }
-        } of response.data.children) {
-          this.list.posts.push({
-            title: title,
-            upvotes: ups,
-            score: score,
-            num_comments: num_comments,
-            created: this.getDatePost(created_utc),
-          });
-        }
-        this.list.count = response.data.dist;
-        return this.list;
-        
-      })
-      
-      
+    return getRequest().then(response => {
+      for (const {
+        data: { title, ups, score, num_comments, created_utc }
+      } of response.data.children) {
+        this.list.posts.push({
+          title: title,
+          upvotes: ups,
+          score: score,
+          num_comments: num_comments,
+          created: this.getDatePost(created_utc)
+        });
+      }
+      this.list.count = response.data.dist;
+      return this.list;
+    });
   }
-  getDatePost(timestamp){
-    const date= new Date(timestamp *1000);
-    let day= date.getDate();
-    let month= ('0' + (date.getMonth() + 1)).slice(-2);
-    let year= date.getFullYear();
-    let hour= date.getHours();
-    let minutes="0" + date.getMinutes();
-    return  `${day}.${month}.${year} ${hour}:${minutes.substr(-2)}`;
+  getDatePost(timestamp) {
+    const date = new Date(timestamp * 1000);
+    let day = date.getDate();
+    let month = ("0" + (date.getMonth() + 1)).slice(-2);
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    return `${day}.${month}.${year} ${hour}:${minutes.substr(-2)}`;
   }
 }
 
